93157f5ff1650ac690fa2acef152330c114253d6
Robert Sirre <robert@cs.nl>
2020-02-13 22:03:58 +0100

    Redirect to Swagger from root

--- inkycal.server/Startup.cs
+++ inkycal.server/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,90 +22,98 @@ namespace InkyCal.Server
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        public static readonly string Intro = @"A web API for <a href=""https://github.com/aceisace/Inky-Calendar"">InkyCal</a>, allows to offload panel-generating complexity to an easier to maintain web service.";
+		public static readonly string Intro = @"A web API for <a href=""https://github.com/aceisace/Inky-Calendar"">InkyCal</a>, allows to offload panel-generating complexity to an easier to maintain web service.";
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
 
-            //services.AddMvc().AddJsonOptions(options =>
-            //{
-            //    options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-            //    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-            //});
+			//services.AddMvc().AddJsonOptions(options =>
+			//{
+			//    options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+			//    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+			//});
 
-            services.AddMvc().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+			services.AddMvc()
+				.AddJsonOptions(options =>
+					options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+				);
 
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = $"InkyCal",
-                    Version = "v1",
-                    Description = Intro,
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Atrejoe",
-                        //Email = "devlog@cs.nl",
-                        Url = new Uri("https://github.com/Atrejoe"),
-                    }
-                });
+			// Register the Swagger generator, defining 1 or more Swagger documents
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Title = $"InkyCal",
+					Version = "v1",
+					Description = Intro,
+					Contact = new OpenApiContact
+					{
+						Name = "Atrejoe",
+						//Email = "devlog@cs.nl",
+						Url = new Uri("https://github.com/Atrejoe"),
+					}
+				});
 
-                //c.DescribeAllEnumsAsStrings();
+				//c.DescribeAllEnumsAsStrings();
 
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+				var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
-                //... and tell Swagger to use those XML comments.
-                c.IncludeXmlComments(xmlPath, true);
-            });
-        }
+				//... and tell Swagger to use those XML comments.
+				c.IncludeXmlComments(xmlPath, true);
+			});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			});
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Inky Calender service V1");
-                c.EnableDeepLinking();
-            });
-        }
-    }
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Inky Calender service V1");
+				c.EnableDeepLinking();
+			});
+
+			var option = new RewriteOptions();
+			option.AddRedirect("^$", "swagger");
+
+			app.UseRewriter(option);
+		}
+	}
 }
