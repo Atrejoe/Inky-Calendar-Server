ff48db0594b6f1fed38915e6108186d5d8c553bc
Robert Sirre <github@robertsirre.nl>
2020-11-25 13:48:03 +0100

    Addressing code quality issue in untitest

--- InkyCal.Utils.Tests/IPanelTests.cs
+++ InkyCal.Utils.Tests/IPanelTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace InkyCal.Utils.Tests
 
 		protected abstract T GetRenderer();
 
+		[SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "Method for delivery test data")]
 		public static IEnumerable<object[]> DisplayModels() {
 			return Enum.GetValues(typeof(DisplayModel))
 				.Cast<DisplayModel>()
