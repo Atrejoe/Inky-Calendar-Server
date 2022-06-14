2f08a33b08a162dc36bd3939f9e77be54fe8f7e6
Robert Sirre <github@robertsirre.nl>
2022-06-14 20:48:14 +0200

    Upgraded Microsoft.* 6.0.5 => 6.0.6

--- InkyCal.Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ InkyCal.Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace InkyCal.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.5")
+                .HasAnnotation("ProductVersion", "6.0.6")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
