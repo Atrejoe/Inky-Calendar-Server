b7af8a95647838922fc24c04ee3aa241461abfe8
Robert Sirre <github@robertsirre.nl>
2023-01-23 20:39:20 +0100

    Added docker vulnerability scanning

--- publish.bat
+++ publish.bat
@@ -1,4 +1,12 @@
-::Publish Docker image, requires write access atreyu/inkycal.server
-docker-compose build
-docker tag inky-calendar-server_web:latest atreyu/inkycal.server:latest
+:: Publish Docker image, requires write access atreyu/inkycal.server
+docker-compose build --pull
+
+:: Tag it as the latest version
+::docker tag inky-calendar-server_web:latest atreyu/inkycal.server:latest
+docker tag docker.io/library/inky-calendar-server-web atreyu/inkycal.server:latest
+
+:: Scan for vulnerabilities
+docker scan atreyu/inkycal.server:latest --file=Dockerfile
+
+:: Publish
 docker push atreyu/inkycal.server:latest
