04dacedfaf3c43372b3fe92c00aea9d0b40843d0
Atreyu <gitkraken@robertsirre.nl>
2023-02-09 11:36:16 +0100

    Upgraded all MS 7.0.0 packages to 7.0.2 (or 3)

--- InkyCal.Data/Migrations/20230209103341_EF_Upgrade_7.0.2.Designer.cs
+++ InkyCal.Data/Migrations/20230209103341_EF_Upgrade_7.0.2.Designer.cs
@@ -4,6 +4,7 @@ using InkyCal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InkyCal.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230209103341_EF_Upgrade_7.0.2")]
+    partial class EFUpgrade702
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InkyCal.Data/Migrations/20230209103341_EF_Upgrade_7.0.2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InkyCal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EFUpgrade702 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			//No model changes, just version number and update to Panel.Modified
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			//No model changes
+		}
+	}
+}
