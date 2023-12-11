33c290a21e6afb38fa49b190c0fd6b12b42a5da0
Atreyu <gitkraken@robertsirre.nl>
2023-12-11 13:55:49 +0100

    Commented out failing debug calender
    Should have been configurable anyway

--- InkyCal.Utils/TestCalendarPanelRenderer.cs
+++ InkyCal.Utils/TestCalendarPanelRenderer.cs
@@ -12,10 +12,10 @@ namespace InkyCal.Utils
 		/// </summary>
 		public const string PublicHolidayCalenderUrl = @"https://calendar.google.com/calendar/ical/en.usa%23holiday%40group.v.calendar.google.com/public/basic.ics";
 
-		/// <summary>
-		/// A public iCal calendar with debug events for demo/test purposes
-		/// </summary>
-		public const string DebugCalenderUrl = @"https://calendar.google.com/calendar/ical/6nqv871neid5l0t7hgk6jgr24c%40group.calendar.google.com/private-c9ab692c99fb55360cbbc28bf8dedb3a/basic.ics";
+		///// <summary>
+		///// A public iCal calendar with debug events for demo/test purposes
+		///// </summary>
+		//public const string DebugCalenderUrl = @"https://calendar.google.com/calendar/ical/6nqv871neid5l0t7hgk6jgr24c%40group.calendar.google.com/private-c9ab692c99fb55360cbbc28bf8dedb3a/basic.ics";
 
 		/// <summary>
 		/// 
@@ -25,8 +25,9 @@ namespace InkyCal.Utils
 				await System.Threading.Tasks.Task.CompletedTask;
 			},
 			iCalUrls: [
-				new Uri(PublicHolidayCalenderUrl), 
-				new Uri(DebugCalenderUrl) 
+				new Uri(PublicHolidayCalenderUrl)
+				//, 
+				//new Uri(DebugCalenderUrl) 
 			],
 			calendars: [],
 			drawMode: Models.CalenderDrawMode.List)
