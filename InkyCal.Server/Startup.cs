8f1d98fe10c211785cb3a6161da0b0bdeefa83f7
Robert Sirre <github@robertsirre.nl>
2024-10-18 22:27:34 +0200

    Added debug info on architecture

--- InkyCal.Server/Startup.cs
+++ InkyCal.Server/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -40,6 +40,10 @@ namespace InkyCal.Server
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public static void ConfigureServices(IServiceCollection services)
 		{
+
+			Console.WriteLine($@"Process architecture: ${System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture}");
+			Console.WriteLine($@"IntPtr.Size: ${IntPtr.Size}");
+
 			services.AddControllers();
 			services.AddHealthChecks()
 				.AddSqlServer(Config.Config.ConnectionString, failureStatus: HealthStatus.Degraded); // Some functions may work, non-user configured (or otherwise cached) methods
