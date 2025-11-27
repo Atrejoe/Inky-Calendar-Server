33153ee3b7501195caba46dcd0c8577ee9b78812
Atreyu <gitkraken@robertsirre.nl>
2025-11-27 23:37:26 +0100

    Upgrade dependencies to v10

--- InkyCal.Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ InkyCal.Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace InkyCal.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.0")
+                .HasAnnotation("ProductVersion", "10.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
