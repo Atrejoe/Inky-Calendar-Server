32c6cea6c19e7d04462b95702da40353bc2dc61c
Atreyu <gitkraken@robertsirre.nl>
2026-07-27 07:28:57 +0200

    Changes test image url
    This should be done properly

--- InkyCal.Utils/TestImagePanelRenderer.cs
+++ InkyCal.Utils/TestImagePanelRenderer.cs
@@ -12,7 +12,7 @@ namespace InkyCal.Utils
 		/// <summary>
 		/// The demo image URL
 		/// </summary>
-		public const string DemoImageUrl = "https://eskipaper.com/images/beautiful-grayscale-wallpaper-1.jpg";
+		public const string DemoImageUrl = "https://cdn.displate.com/artwork/2026-03-07/47160f6f-1621-49ae-9674-29bcb4a08ac4.jpg";
 
 		/// <summary>
 		/// Creates the demo panel, uses <see cref="DemoImageUrl"/>
